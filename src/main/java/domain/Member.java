41129b2cd3c63490b6347a51c5cb20495a213f63
meridaKim <sujin7187@naver.com>
2023-04-02 14:59:49 +0900

    [ADD]JNLE-9 회원(member) JPA 코드 추가

---
+++ src/main/java/domain/Member.java
@@ -0,0 +1,28 @@
+package domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class Member{
+    @Id @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private String user_id;
+    private String user_name;
+    private String user_pw;
+    private String user_address;
+    @Column(name = "PET_ID")
+    private long pet_id;
+    @Column(name ="PAYMENT_LIST_ID")
+    private String payment_list_id;
+
+
+
+}
