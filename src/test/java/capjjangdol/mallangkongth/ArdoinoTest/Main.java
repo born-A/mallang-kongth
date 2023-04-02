5bb04c256ab916b98a1fa67bd42870880ddc98bc
onejm <jeongmin0324@gmail.com>
2023-04-02 12:37:09 +0900

    [ADD] rxtx 라이브러리 추가 -2

---
+++ src/test/java/capjjangdol/mallangkongth/ArdoinoTest/Main.java
@@ -0,0 +1,17 @@
+package capjjangdol.mallangkongth.ArdoinoTest;
+
+
+
+public class Main {
+    public static void main(String[] args)
+    {
+        try {
+
+            (new Serial()).connect("COM3");
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
