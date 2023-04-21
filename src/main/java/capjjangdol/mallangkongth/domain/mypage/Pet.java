1ac0a5c38601f5595037affd0e81958964f72805
bona <ra_ari@naver.com>
2023-04-21 09:59:01 +0900

    [UPDATE] properties -> yml 변경

--- src/main/java/capjjangdol/mallangkongth/domain/mypage/Pet.java
+++ src/main/java/capjjangdol/mallangkongth/domain/mypage/Pet.java
@@ -1,18 +1,24 @@
 package capjjangdol.mallangkongth.domain.mypage;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Getter @Setter
 public class Pet {
     @Id
     @GeneratedValue
-    @Column(name = "PET_ID")
+    @Column(name = "pet_id")
     private Long id;
 
+    @Column(name = "pet_name")
+    private String name;
     @ManyToOne
-    @JoinColumn(name = "MEMBER_ID")
+    @JoinColumn(name = "member_id")
     private Member member;
 
 
