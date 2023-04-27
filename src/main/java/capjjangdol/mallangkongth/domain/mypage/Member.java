8083836905edbb93ae870642e304cd960679b590
meridaKim <sujin7187@naver.com>
2023-04-27 12:56:54 +0900

    [CHANGE]member getter/setter

--- src/main/java/capjjangdol/mallangkongth/domain/mypage/Member.java
+++ src/main/java/capjjangdol/mallangkongth/domain/mypage/Member.java
@@ -1,6 +1,9 @@
 package capjjangdol.mallangkongth.domain.mypage;
 
+import jdk.nashorn.internal.runtime.Debug;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -8,13 +11,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Getter
-@Setter
-public class Member{
+@Builder
+@RequiredArgsConstructor
+@Getter @Setter
+public class Member {
     @Id @GeneratedValue
     @Column(name = "member_id")
     private Long id;
+
+    @Column(nullable = false, length = 50, unique = true)
+    private String user_id;
+    @Column(nullable = false,length = 30, unique = true)
     private String name;
+    @Column(length = 100)
     private String pw;
 
     @Embedded
@@ -24,10 +33,37 @@ public class Member{
 //    private List<Pet> petList = new ArrayList<>();
 
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private RoleType roleType;
 
 //    @OneToMany(mappedBy = "member")
 //    private List<Orders> ordersList = new ArrayList<>();
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPw() {
+        return pw;
+    }
+
+    public void setPw(String pw) {
+        this.pw = pw;
+    }
+
+
 
 ////    @ManyToMany
 //    @Column(name ="ORDER_ID")
