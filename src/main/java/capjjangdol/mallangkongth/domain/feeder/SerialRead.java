0f155d055ed5a4f98e0b5955936d31ace7f54f22
onejm <jeongmin0324@gmail.com>
2023-05-08 00:36:04 +0900

    [ADD] WaterLevelSensor값 저장 -7

--- src/main/java/capjjangdol/mallangkongth/domain/feeder/SerialRead.java
+++ src/main/java/capjjangdol/mallangkongth/domain/feeder/SerialRead.java
@@ -4,19 +4,17 @@ import capjjangdol.mallangkongth.repository.WaterLevelRepository;
 import capjjangdol.mallangkongth.service.PetService;
 import capjjangdol.mallangkongth.service.WaterLevelService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.InputStream;
 
 //	값을 읽는 클래스로, 이는 Thread로 구현해야 한다.
-
 public class SerialRead implements Runnable
 {
     InputStream in;
 
     public SerialRead(InputStream in){this.in = in;}
-
-    static WaterLevelRepository waterLevelRepository;
 
     @Override
     public void run()
@@ -37,8 +35,9 @@ public class SerialRead implements Runnable
                 if (len == 5 && s.charAt(0) == 'w'&& !s.contains("h")){//받아오는 값의 자릿수가 5자리일 때만 출력, 값 중간에 - 나오지 않도록 함
                     s = s.replaceAll("w", ""); //데이터 앞에 붙은 w 지우기
                     WaterLevel waterLevel = new WaterLevel();
-                    waterLevel.setWaterLevel(Integer.parseInt(s)); //int 값으로 변환하여 넣기
-                    waterLevelRepository.save(waterLevel);
+                    waterLevel.setWaterLevel(Integer.parseInt(s));
+                    WaterData waterData =new WaterData();
+                    waterData.saveWater(waterLevel);//int 값으로 변환하여 넣기
                 } else if (len == 5 && s.charAt(0) == 'h' && !s.contains("w")) {
                     s = s.replaceAll("h", ""); //데이터 앞에 붙은 h 지우기
                     Integer.parseInt(s); //데이터 Int 값으로 변경
--- src/main/java/capjjangdol/mallangkongth/domain/feeder/WaterData.java
+++ src/main/java/capjjangdol/mallangkongth/domain/feeder/WaterData.java
@@ -2,11 +2,19 @@ package capjjangdol.mallangkongth.domain.feeder;
 
 
 import capjjangdol.mallangkongth.repository.WaterLevelRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
+@Component
 public class WaterData {
+
+    @Autowired
     WaterLevelRepository waterLevelRepository;
 
-    public WaterData(WaterLevel waterLevel)
+
+    public void saveWater(WaterLevel waterLevel)
     {
         waterLevelRepository.save(waterLevel);
     }
