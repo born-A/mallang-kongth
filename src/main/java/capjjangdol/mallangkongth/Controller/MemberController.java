824438694da3295ed3cfdd5b51f9920640b64da9
meridaKim <sujin7187@naver.com>
2023-05-12 13:33:43 +0900

    [CHANGE]customerDetailsService 이름 변경

--- src/main/java/capjjangdol/mallangkongth/Controller/MemberController.java
+++ src/main/java/capjjangdol/mallangkongth/Controller/MemberController.java
@@ -1,10 +1,9 @@
 package capjjangdol.mallangkongth.Controller;
 
-import capjjangdol.mallangkongth.domain.mypage.ChangePwReqDto;
-import capjjangdol.mallangkongth.domain.mypage.MemberReqDto;
-import capjjangdol.mallangkongth.domain.mypage.MemberResDto;
+import Validator.CheckUser_idValidator;
 import capjjangdol.mallangkongth.service.MemberService;
-import lombok.RequiredArgsConstructor;
+import capjjangdol.mallangkongth.dto.MemberDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
