cd841a19a4637e7458c5ee25bb806be90a90c00d
onejm <jeongmin0324@gmail.com>
2023-05-12 11:28:37 +0900

    [ADD] waterBowl 객체 수정

--- src/main/java/capjjangdol/mallangkongth/domain/rearing/WaterBowl.java
+++ src/main/java/capjjangdol/mallangkongth/domain/rearing/WaterBowl.java
@@ -2,20 +2,28 @@ package capjjangdol.mallangkongth.domain.rearing;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @Setter
 public class WaterBowl {
     @Id @GeneratedValue
-    private Long id;
+    private String id;
 
     private Long remaining;
 
+    private Long beforeEatingAmount;
+
+    private Long currentEatingAmount;
     @Column(name = "setting_amount")
     private Long settingAmount;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private LocalDateTime insertDate;
 
     public Long eatingAmount() {
         return this.settingAmount - this.remaining;
--- src/test/java/capjjangdol/mallangkongth/RepositoryTest/RearingRepositoryTest.java
+++ src/test/java/capjjangdol/mallangkongth/RepositoryTest/RearingRepositoryTest.java
@@ -1,7 +1,6 @@
 package capjjangdol.mallangkongth.RepositoryTest;
 
 import capjjangdol.mallangkongth.MallangKongthApplicationTests;
-import capjjangdol.mallangkongth.domain.rearing.RearingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
