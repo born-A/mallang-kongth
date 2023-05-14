86a0158718f9f4d93df43372cc95f233fe30e7a8
onejm <jeongmin0324@gmail.com>
2023-05-14 18:42:12 +0900

    [CHANGE] 쿼리 수정 -1

--- src/main/java/capjjangdol/mallangkongth/repository/WaterBowlRepository.java
+++ src/main/java/capjjangdol/mallangkongth/repository/WaterBowlRepository.java
@@ -6,13 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface WaterBowlRepository extends JpaRepository<WaterBowl, String> {
-    @Query(value = "SELECT setting_amount FROM water_bowl WHERE id = (SELECT MAX(id) FROM water_bowl )")
+    @Query(value = "SELECT w1.settingAmount FROM WaterBowl w1 WHERE w1.id = (SELECT MAX(w2.id) FROM WaterBowl w2 )")
     Integer findSettingAmount();
-    @Query(value = "SELECT before_eating_amount FROM water_bowl WHERE id = (SELECT MAX(id) FROM water_bowl )")
+    @Query(value = "SELECT w1.beforeEatingAmount FROM WaterBowl w1 WHERE w1.id = (SELECT MAX(w2.id) FROM WaterBowl w2 )")
     Integer findBeforeEatingAmount();
-
-    @Query(value = "SELECT current_eating_amount FROM water_bowl WHERE id = (SELECT MAX(id) FROM water_bowl )")
+    @Query(value = "SELECT w1.currentEatingAmount FROM WaterBowl w1 WHERE w1.id = (SELECT MAX(w2.id) FROM WaterBowl w2 )")
     Integer findCurrentEatingAmount();
-    @Query(value = "SELECT remaining FROM water_bowl WHERE id = (SELECT MAX(id) FROM water_bowl )")
+    @Query(value = "SELECT w1.remaining FROM WaterBowl w1 WHERE w1.id = (SELECT MAX(w2.id) FROM WaterBowl w2 )")
     Integer findremaining();
 }
--- src/main/java/capjjangdol/mallangkongth/repository/WaterNoteRepository.java
+++ src/main/java/capjjangdol/mallangkongth/repository/WaterNoteRepository.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface WaterNoteRepository extends JpaRepository<WaterNote, Long> {
     //가장 마지막에 입력된 수위센서 값
-    @Query(value = "SELECT water_amount FROM water_note WHERE insert_time = (SELECT MAX(insert_time) FROM water_note )")
+    @Query(value = "SELECT w1.insertTime FROM WaterNote w1 WHERE w1.id = (SELECT MAX(w2.id) FROM WaterNote w2 )")
     Integer findWaterAmountLatestInsertTime();
 }
