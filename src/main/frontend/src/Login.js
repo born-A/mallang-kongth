402988c4b386db1e61fa1b56a3ff33231ce5688c
choiminseoo <minseoo5290@gmail.com>
2023-05-19 11:41:47 +0900

    [Change] 로그인 화면 수정 -1

--- src/main/frontend/src/Login.js
+++ src/main/frontend/src/Login.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import { useState } from 'react';
-import { Link, Route, Routes } from 'react-router-dom';
-import './Login.css';
+import React, {useState} from "react";
+import "./Login.css";
 
-function Login() {
+export default function Login() {
+
     const [id, setId] = useState('');
     const [password, setPassword] = useState('');
 
@@ -21,34 +20,73 @@ function Login() {
         // 여기서는 console.log로만 출력했지만, 이 부분을 실제 로그인 로직으로 대체하면 됩니다.
     };
 
-    return (
-        <div className="login-container">
-            <form onSubmit={handleSubmit}>
-                <h1>말 랑 콩 떡</h1>
-                <h2>만나서 반가워요 !</h2>
-                <label>ID</label>
-                <input type="text" value={id} onChange={handleIdChange} />
-                <label>Password</label>
-                <input type="password" value={password} onChange={handlePasswordChange} />
-                <button type="submit" className="login">로그인</button>
-                <div>
-                    <h3>↓다른 아이디로 로그인하기↓</h3>
-                </div>
-                <div className="container">
-                    <div className="row">
-                        <div className="col-12">
-                            <div>
-                                <button type="button" className="kakao">카카오톡</button>
-                                <button type="button" className="naver">네이버</button>
-                                <button type="button" className="google">구글</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </form>
-            <p><Link to={"/Signup"}>아직 계정이 없으신가요?</Link></p>
-        </div>
-    );
-}
+    const handleKakaoLogin = () => {
+        // 로그인 처리 로직 작성
+        // ...
+    };
 
-export default Login;
+    // 네이버로 로그인
+    const handleNaverLogin = () => {
+        // 로그인 처리 로직 작성
+        // ...
+    };
+
+    // 구글로 로그인
+    const handleGoogleLogin = () => {
+        // 로그인 처리 로직 작성
+        // ...
+    };
+
+
+
+    return(
+        <div className="background">
+            <div className="box">
+                {/* 헤더부분 */}
+                <div className="header">
+                    <h1>말랑콩떡</h1>
+                </div>
+
+                <div className="box2">
+
+                    {/* 인사말*/}
+                    <div className="welcome">
+                        <span>만나서 반가워요!</span>
+                    </div>
+
+                    {/* 로그인하기 */}
+                    <div className="loginInput">
+                        <span>아이디 <input className="Id" type="email" value={id} onChange={handleIdChange} /></span>
+                        <span>비밀번호 <input type="password" value={password} onChange={handlePasswordChange} /></span>
+                    </div>
+                    <div className="login">
+                        <button className="login-button" onChange={handleSubmit}> 로그인</button>
+                    </div>
+
+                    <div className="Integration-login">
+                        <span>다른 아이디로 로그인하기</span>
+                    </div>
+
+                    <div className="logo">
+                        <button className="kakao" onClick={handleKakaoLogin}>
+                            카카오톡으로 로그인하기
+                        </button>
+                        <button className="naver" onClick={handleNaverLogin}>
+                            네이버로 로그인하기
+                        </button>
+                        <button className="google" onClick={handleGoogleLogin}>
+                            구글로 로그인하기
+                        </button>
+                    </div>
+
+                    <div className="go-signup">
+                        <span>아직 계정이 없으신가요?</span>
+                        회원가입
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
+
+}
