d6d9b3f8ebb80b220315038a8915aa4bb6966307
bona <ra_ari@naver.com>
2023-05-24 18:41:01 +0900

    [ADD] 주문 기능 도메인,리포,서비스 구현, 멤버에 주문 기록 양방향 매핑 추가

--- src/main/java/capjjangdol/mallangkongth/domain/mypage/Item.java
+++ src/main/java/capjjangdol/mallangkongth/domain/mypage/Item.java
@@ -10,20 +10,36 @@ import java.util.List;
 
 @Entity
 @Getter @Setter
-public class Item {
+public abstract class Item {
     @Id
     @GeneratedValue
     @Column(name = "item_id")
     private Long id;
-
-    @OneToMany(mappedBy = "item")
-    private List<OrderItem> orderItems = new ArrayList<>();
-
+//
+//    @OneToMany(mappedBy = "item")
+//    private List<OrderItem> orderItems = new ArrayList<>();
+//
     private String name;
-    private String category;
-    private Integer price;
-    private Integer stock; //???
-    private String information; //????
-    private String company; //???
+//    private String category;
+
+//    private String information; //????
+//    private String company; //???
+
+    private int price;
+    private int stockQuantity;
+    @ManyToMany(mappedBy = "items")
+    private List<ItemCategory> categories = new ArrayList<ItemCategory>();
+
+    //==비즈니스 로직==//
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock > 0) {
+            this.stockQuantity = restStock;
+        }
+
+    }
 
 }
