98d51504c74056fcc8fd400b139008e466baf0f0
bona <ra_ari@naver.com>
2023-05-29 01:17:31 +0900

    [UPDATE] Pet 몸무게, 생년월일,성별,견종 필드 추가, 타임리프 페이지 수정

--- src/main/java/capjjangdol/mallangkongth/domain/mypage/PetForm.java
+++ src/main/java/capjjangdol/mallangkongth/domain/mypage/PetForm.java
@@ -2,10 +2,23 @@ package capjjangdol.mallangkongth.domain.mypage;
 
 import lombok.Getter;
 import lombok.Setter;
+
+import javax.persistence.Column;
 import javax.validation.constraints.NotEmpty;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Getter @Setter
 public class PetForm {
     @NotEmpty(message = "반려동물 이름은 필수 입력 사항입니다.")
     private String name;
+
+
+    private String birthday;
+
+    private double weight;
+
+    private boolean gender; // 성별 (true: 남성, false: 여성)
+
+    private String breed;
 }
