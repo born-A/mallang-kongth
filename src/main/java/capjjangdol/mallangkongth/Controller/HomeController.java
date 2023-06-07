0afacd751f8c4efdced08e9c5e11e8ef0db91a3d
bona <ra_ari@naver.com>
2023-06-08 01:11:05 +0900

    [ADD] 메인 페이지 구현

--- src/main/java/capjjangdol/mallangkongth/Controller/HomeController.java
+++ src/main/java/capjjangdol/mallangkongth/Controller/HomeController.java
@@ -2,6 +2,8 @@ package capjjangdol.mallangkongth.Controller;
 
 import capjjangdol.mallangkongth.config.SessionManager;
 import capjjangdol.mallangkongth.domain.mypage.LoginForm;
+import capjjangdol.mallangkongth.domain.mypage.Member;
+import capjjangdol.mallangkongth.service.MemberService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,7 +15,10 @@ import org.springframework.web.bind.annotation.SessionAttribute;
 @Slf4j
 public class HomeController {
 
+
     private SessionManager sessionManager;
+
+
     /*@GetMapping("/")
     public String home(HttpServletRequest httpServletRequest, Model model){
         log.info("home controller");
@@ -25,16 +30,16 @@ public class HomeController {
     }*/
 
     @RequestMapping ("/")
-    public String homeLogin(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false)Long memberId, Model model){
-
+    public String homeLogin(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false)Member member, Model model){
         /** 로그인 페이지로 이동 **/
-        if(memberId == null) {
+        if(member == null) {
             model.addAttribute("loginForm", new LoginForm());
             return "logins/sign-in";
         }
 
         /** 메인 페이지로 이동 **/
-        return "home";
+        model.addAttribute("member", member);
+        return "index";
     }
 //        @RequestMapping("/")
 //    public String home(){
