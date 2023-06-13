d013ec72db373f6d1830fb6f5eb17b5053a4f3a9
bona <ra_ari@naver.com>
2023-06-14 00:46:47 +0900

    [UPDATE] OrderController, orderForm

--- src/main/java/capjjangdol/mallangkongth/Controller/OrderController.java
+++ src/main/java/capjjangdol/mallangkongth/Controller/OrderController.java
@@ -1,6 +1,8 @@
 package capjjangdol.mallangkongth.Controller;
 
+import capjjangdol.mallangkongth.domain.file.FileEntity;
 import capjjangdol.mallangkongth.domain.mypage.*;
+import capjjangdol.mallangkongth.repository.FileRepository;
 import capjjangdol.mallangkongth.repository.MemberRepository;
 import capjjangdol.mallangkongth.service.CategoryService;
 import capjjangdol.mallangkongth.service.ItemService;
@@ -20,20 +22,24 @@ public class OrderController {
     private final OrderService orderService;
     private final MemberService memberService;
     private final ItemService itemService;
-
     private final CategoryService categoryService;
 
+    private final FileRepository fileRepository;
+
     @GetMapping(value = "/item/order/{id}")
-    public String createForm(@PathVariable("id") Long id,Model model) {
+    public String createForm(@SessionAttribute(name= SessionConst.LOGIN_MEMBER,required = false)Member member,@PathVariable("id") Long id,Model model) {
         List<Member> members = memberService.findAll();
         Item item = itemService.findOne(id);
         List<CategoryOfItem> categories = categoryService.findCategories();
+
+        model.addAttribute("member", member);
         model.addAttribute("members", members);
         model.addAttribute("item", item);
         model.addAttribute("categories", categories); //카테고리
+        List<FileEntity> files = fileRepository.findAll();
+        model.addAttribute("all",files);
         return "order/orderForm";
     }
-
 
     @PostMapping(value = "/item/order/{id}")
         public String order(@SessionAttribute(name= SessionConst.LOGIN_MEMBER,required = false)Member member,
